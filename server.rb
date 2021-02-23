cb05924f9da6ee4b1f2d271d529ddc1d5513f170
mrbigass <wetsand.wfs@gmail.com>
2021-02-23 11:33:31 +0900

    perse request like web server (puma, unicorn)

--- server.rb
+++ server.rb
@@ -1,25 +1,52 @@
 require "socket"
 
+class Application
+  def call(env)
+    [200, {}, ["Hello World: " + Time.now.to_s]]
+  end
+end
+
+app = Application.new
+
 port = 3000
 server = TCPServer.open(port)
-
 
 loop do
   # wait for connection
   socket = server.accept
 
-  puts "Got a connection"
+  if match = socket.gets.chomp.match(/^(?<verb>[A-Z]*) (?<path>[^ ]*) (?<ver>.*)$/)
+    # make a hash with the request information
+    env = {
+      "REQUEST_METHOD" => match[:verb],
+      "PATH_INFO" => match[:path],
+      "HTTP_VERSION" => match[:ver],
+    }
 
-  loop do
-    line = socket.readline.chomp
-    puts "Read: #{line}"
-    if line.bytesize == 0
-      socket.write "HTTP/1.1 200 OK\r\n"
-      socket.write "\r\n"
-      socket.write "Hello world\r\n"
-      socket.close
-      break
+    while line = socket.gets.chomp
+      break if line.bytesize == 0
+      key, value = line.split(": ")
+      env[key] = value
     end
+
+    # call app with request information
+    response = app.call(env)
+
+    status = response[0]
+    headers = response[1]
+    body   = response[2]
+
+    socket.write "HTTP/1.1 #{status} OK\r\n"
+
+    headers.each do |key, value|
+      socket.write "#{key}: #{value}"
+    end
+
+    socket.write "\r\n"
+
+    body.each { |part| socket.write part }
+    socket.close
+  else
+    socket.close
   end
 end
-
