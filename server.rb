d0976797e791e496585949db40209f8bdb10f02d
mrbigass <wetsand.wfs@gmail.com>
2021-02-23 11:00:38 +0900

    make waiting status

--- server.rb
+++ server.rb
@@ -3,17 +3,23 @@ require "socket"
 port = 3000
 server = TCPServer.open(port)
 
-# wait for connection
-socket = server.accept
 
 loop do
-  line = socket.readline.chomp
-  puts "Read: #{line}"
-  if line.bytesize == 0
-    socket.write "HTTP/1.1 200 OK\r\n"
-    socket.write "\r\n"
-    socket.write "Hello world\r\n"
-    socket.close
-    break
+  # wait for connection
+  socket = server.accept
+
+  puts "Got a connection"
+
+  loop do
+    line = socket.readline.chomp
+    puts "Read: #{line}"
+    if line.bytesize == 0
+      socket.write "HTTP/1.1 200 OK\r\n"
+      socket.write "\r\n"
+      socket.write "Hello world\r\n"
+      socket.close
+      break
+    end
   end
 end
+
